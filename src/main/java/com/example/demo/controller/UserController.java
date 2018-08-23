71e49c8eb6a4d49a9da3810734a33a55ed63b3b8
jinben <jinben01@inspur.com>
2018-08-23 21:00:53 +0800

    更新配置文件

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +9,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
 
+    @Value("${user.name}") // 居然自己获取系统的
+    private String name;
+
+    @Value("${user.time}")
+    private String time;
+
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String sayHello() {
-        return "Hello World";
+        return "Hello " + name + "\n time: " + time;
     }
 
 }
